7daa4ce5bad2c55c2033d15926f913950ed401dd
DaniloJacome <whichy22@gmail.com>
2023-08-25 11:19:54 -0500

    Cambios Int y Float

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -3,6 +3,8 @@ saludar=function(){
     let nombre=recuperarTexto("txtNombre");
     //recuperar el valor de la caja de texto txtNombre
     let apellido=recuperarTexto("txtApellido");
+    let edad=recuperarInt("txtEdad");
+    let estatura=recuperarFloat("txtEstatura");
 } 
 recuperarTexto=function(idComponente){
     let componente;
@@ -10,4 +12,14 @@ recuperarTexto=function(idComponente){
     componente=document.getElementById(idComponente);
     valorIngresado=componente.value;
     return valorIngresado;
+}
+recuperarInt=function(idComponente){
+    let valorCaja=recuperarTexto(idComponente);
+    let valorEntero=parseInt(valorCaja);
+    return valorEntero;
+}
+recuperarFloat=function(idComponente){
+    let valorCaja=recuperarTexto(idComponente);
+    let valorFlotante=parseFloat(valorCaja);
+    return valorFlotante;
 }
