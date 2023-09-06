39240304474d52a9ffae06aac3be849af026c95d
DaniloJacome <whichy22@gmail.com>
2023-09-06 17:51:51 -0500

    Ejercicio Cadenas Agregado

--- bucles/ejercicioCadenas.js
+++ bucles/ejercicioCadenas.js
@@ -27,3 +27,31 @@ ejecutarPrueba2 = function() {
     mensaje = recuperarTexto("txtCadena");
     recorrerCadenaInvertida(mensaje);
 }
+
+buscarLetra = function(cadena,letra){
+    let letraIterada;
+    let existeLetra=false;
+    for(let i=0;i<cadena.length;i++){
+        letraIterada=cadena.charAt(i);
+        if(letraIterada==letra){
+            existeLetra=true;
+        }
+    }
+    if(existeLetra==true){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+contarMayusculas = function(cadena){
+    let letra;
+    let contadorMayusculas;
+    for(let i=0;i<cadena.length;i++){
+        letra=cadena.charAt(i);
+        if(esMayuscula(letra)){
+            contadorMayusculas=contadorMayusculas+1;
+        }
+    }
+    console.log(contadorMayusculas);
+}
