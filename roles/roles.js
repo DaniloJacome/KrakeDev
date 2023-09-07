1cc6a3300cd67df9683376d0416cb52d763d976f
DaniloJacome <whichy22@gmail.com>
2023-09-07 16:20:58 -0500

    MOSTRASR EMPLEADOS Y TABLA

--- roles/roles.js
+++ roles/roles.js
@@ -1,3 +1,9 @@
+let empleados = [
+    { cedula: "1253587565", nombre: "DAVID", apellido: "SUAREZ", sueldo: 450.0 },
+    { cedula: "5165484812", nombre: "ALEJANDRO", apellido: "DAVALOS", sueldo: 500.0, },
+    { cedula: "2689562264", nombre: "CRISTIAN", apellido: "JACOME", sueldo: 1250.0, },
+]
+
 let esNuevo = false;
 
 const mostrarOpcionEmpleado = function () {
@@ -22,4 +28,39 @@ const mostrarOpcionResumen = function () {
     ocultarComponente("divRol");
     mostrarRoles();
     mostrarTotales();
+}
+
+const mostrarEmpleados = function () {
+    let elementoEmpleados;
+    let cmpTabla = document.getElementById("tablaEmpleados");
+    let contenidoTabla =
+        "<table>" +
+        "<tr>" +
+        "<th>CEDULA</th>" +
+        "<th>NOMBRE</th>" +
+        "<th>APELLIDO</th>" +
+        "<th>SUELDO</th>" +
+        "</tr>";
+
+    for (let i = 0; i < empleados.length; i++) {
+        elementoEmpleados = empleados[i];
+        contenidoTabla +=
+            "<tr>" +
+            "<td>" + elementoEmpleados.cedula + "</td>" +
+            "<td>" + elementoEmpleados.nombre + "</td>" +
+            "<td>" + elementoEmpleados.apellido + "</td>" +
+            "<td>" + elementoEmpleados.sueldo +
+            "</td>" +
+            "</tr>";
+    }
+    contenidoTabla += "</table>";
+    cmpTabla.innerHTML = contenidoTabla;
+}
+
+const deshabilitarComponentesDeIngreso = function () {
+    deshabilitarComponente("txtCedula");
+    deshabilitarComponente("txtNombre");
+    deshabilitarComponente("txtApellido");
+    deshabilitarComponente("txtSueldo");
+    deshabilitarComponente("btnGuardar");
 }
