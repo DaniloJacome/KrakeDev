8576d10e7a895eaddf8babe909be3fec58a7c43c
DaniloJacome <whichy22@gmail.com>
2023-09-07 15:34:19 -0500

    FUNCION RESUMEN, EMPLEADO Y ROL

---
+++ roles/roles.js
@@ -0,0 +1,25 @@
+let esNuevo = false;
+
+const mostrarOpcionEmpleado = function () {
+    mostrarComponente("divEmpleado");
+    ocultarComponente("divRol");
+    ocultarComponente("divResumen");
+    mostrarEmpleados();
+
+}
+
+const mostrarOpcionRol = function () {
+    mostrarComponente("divRol");
+    ocultarComponente("divEmpleado");
+    ocultarComponente("divResumen");
+    deshabilitarComponente("btnGuadarRol");
+    mostrarRoles();
+}
+
+const mostrarOpcionResumen = function () {
+    mostrarComponente("divResumen");
+    ocultarComponente("divEmpleado");
+    ocultarComponente("divRol");
+    mostrarRoles();
+    mostrarTotales();
+}
